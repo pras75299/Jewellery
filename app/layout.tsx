8e8d7931bff53216ada5946360452a75f9604291
pras75299 <singh.prashantking@gmail.com>
2025-11-02 15:59:33 +0530

    feat: Add toast notifications to app layout
    - Add Sonner Toaster component for global toast notifications
    - Enable rich colors for better visual feedback

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster position="top-right" richColors />
+        <Toaster position="bottom-right" richColors />
       </body>
     </html>
   );
